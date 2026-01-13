b40136e1bc0b7280d92ec4543a6ef245241dee32
Manav Lathiya <manavlathiya11@gmail.com>
2026-01-13 18:22:21 -0500

    chore: add eslint tsconfig to lint config files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,15 +2,12 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
+    project: './tsconfig.eslint.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint', 'prettier'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   env: {
     node: true,
     jest: true,
